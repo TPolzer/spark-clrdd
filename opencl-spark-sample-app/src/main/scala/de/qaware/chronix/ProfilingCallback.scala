1b332871054d590f0d5c4690ec36ae397528d2f6
Tobias Polzer <tobias.polzer@fau.de>
2016-12-08 23:35:14 +0100

    remove logging from ProfilingCallback

--- opencl-spark-sample-app/src/main/scala/de/qaware/chronix/ProfilingCallback.scala
+++ opencl-spark-sample-app/src/main/scala/de/qaware/chronix/ProfilingCallback.scala
@@ -14,15 +14,11 @@ class ProfilingCallback(ready: cl_event, counter: Option[AtomicLong]) extends Ev
   override def function(ready: cl_event, command_exec_callback_type: Int, user_data: AnyRef) = {
 	try {
 	  val profiling = new Array[Long](1)
-	  clGetEventProfilingInfo(ready, CL_PROFILING_COMMAND_QUEUED, Sizeof.cl_ulong, Pointer.to(profiling), null)
-	  val queued = profiling(0)
 	  clGetEventProfilingInfo(ready, CL_PROFILING_COMMAND_START, Sizeof.cl_ulong, Pointer.to(profiling), null)
 	  val started = profiling(0)
-	  LoggerFactory.getLogger(getClass).trace("{} lingered in queue for {}ms", ready, (started-queued)/1e6)
 	  clGetEventProfilingInfo(ready, CL_PROFILING_COMMAND_END, Sizeof.cl_ulong, Pointer.to(profiling), null)
 	  val ended = profiling(0)
 	  counter.foreach(_.addAndGet(ended - started))
-	  LoggerFactory.getLogger(getClass).trace("{} took {}ms", ready, (ended-started)/1e6)
 	} finally safeReleaseEvent(ready)
   }
 }
