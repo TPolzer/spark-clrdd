5dab2bab9332642d4427a409141c50c9b51f555f
Tobias Polzer <tobias.polzer@fau.de>
2016-12-17 17:53:01 +0100

    Add possibility to merge benchmark runs

--- opencl-spark-sample-app/benchmarks/benchmarktable.cpp
+++ opencl-spark-sample-app/benchmarks/benchmarktable.cpp
@@ -3,11 +3,13 @@
 #include <regex>
 #include <sstream>
 #include <set>
+#include <iomanip>
 
 using namespace std;
 using ll = long long;
 
 int main() {
+	cout << fixed << setprecision(3);
 	regex header("Benchmark.*cpu.*size.*Mode.*Cnt.*Score.*Error.*Units");
 	string line;
 	getline(cin, line);
@@ -16,18 +18,24 @@ int main() {
 		return 1;
 	}
 	set<string> categories;
-	map<ll, map<string, string>> table;
+	map<ll, map<string, pair<ll, double>>> table;
 	while(getline(cin, line)) {
+		if(regex_match(line, header))
+			continue;
 		stringstream ss(line);
 		string _;
 		getline(ss, _, '.'); //get rid of BenchmarkClass
-		string name, cpu, score;
-		ll size;
-		ss >> name >> cpu >> size >> _ >> _ >> score;
+		string name, cpu;
+		double score;
+		ll size, count;
+		ss >> name >> cpu >> size >> _ >> count >> score;
 		if(cpu == "false")
 			name += "_gpu";
 		categories.insert(name);
-		table[size][name] = score;
+		auto old = table[size][name];
+		ll ncount = old.first + count;
+		double nscore = (old.second * old.first + score * count) / ncount;
+		table[size][name] = make_pair(ncount, nscore);
 	}
 	cout << "size";
 	for(const string &head : categories)
@@ -41,7 +49,7 @@ int main() {
 			if(it == end(e.second))
 				cout << "nan";
 			else
-				cout << it->second;
+				cout << it->second.second;
 		}
 		cout << endl;
 	}
